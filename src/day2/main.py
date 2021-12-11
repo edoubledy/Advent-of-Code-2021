5eceb2e1e60fd54bcb23ff2ac2d2ee13bbe2decd
edoubledy <eddy@eddyhaigh.io>
2021-12-11 10:17:29 -0800

    Complete day 2

--- src/day2/main.py
+++ src/day2/main.py
@@ -16,5 +16,26 @@ def answer_one():
     
     print(f"Answer One: {horizontal_position * depth}")
 
+def answer_two():
+    with open("./src/day2/data/input") as f:
+        lines = [line.strip().split(' ') for line in f]
+
+    horizontal_position = 0
+    depth = 0
+    aim = 0
+
+    for line in lines:
+        match line:
+            case ['forward', x]:
+                horizontal_position += int(x)
+                depth += int(x) * aim
+            case ['down', x]:
+                aim += int(x)
+            case ['up', x]:
+                aim -= int(x)
+    
+    print(f"Answer One: {horizontal_position * depth}")
+
 if __name__ == '__main__':
     answer_one()
+    answer_two()
